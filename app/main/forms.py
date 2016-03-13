9fb39d822bf1b218b09e5f761bc87ac60d99a3c2
David-Guo <guodw3@mail2.sysu.edu.cn>
2016-03-14 07:38:50 +0800

    complete login and register

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,16 +8,33 @@ Created on Sun Mar 13 22:03:00 2016
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
     SubmitField, PasswordField
-from wtforms.validators import Required, Length, Email, Regexp
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
+from ..models import User
 
 class PostForm(Form):
     body = TextAreaField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
     
 class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField(u'用户名或Email', validators=[Required(), Length(1, 64),
                                              Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    password = PasswordField(u'密码', validators=[Required()])
+    remember_me = BooleanField(u'记住我')
+    submit = SubmitField(u'提交')
+    
+
+class RegistrationForm(Form):
+    username = StringField(u'用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    password = PasswordField(u'密码', validators=[
+        Required(), EqualTo('password2', message=u'两次输入密码必须匹配')])
+    password2 = PasswordField(u'确认密码', validators=[Required()])
+    submit = SubmitField(u'提交')
+
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
--- app/main/views.py
+++ app/main/views.py
@@ -5,21 +5,55 @@ Created on Sun Mar 13 22:24:13 2016
 @author: david
 """
 
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, abort, flash, request
 from . import main
-from .forms import PostForm, LoginForm
-from flask.ext.login import current_user
-from ..models import Post
+from .forms import PostForm, LoginForm, RegistrationForm
+from flask.ext.login import current_user, login_user
+from ..models import Post, User
 from .. import db
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
+    form = PostForm()
     if form.validate_on_submit():
         post = Post(body=form.body.data,
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-#    posts = Post.query.order_by(Post.timestamp.desc()).all()
-#    posts = None
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
+    
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
+    
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            # 在用户会话中标记用户为已登录
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash(u'无效的用户名或密码')
     return render_template('login.html', form=form)
+
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(u'注册成功！')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+    
+
